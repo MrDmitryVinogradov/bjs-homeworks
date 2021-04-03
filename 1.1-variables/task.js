9b21bce9b24215d44f37b097c8c493a85222f00f
Dmitry Vinogradov <pierrebernard@MacBook-Air-Pierre-Bernard.local>
2021-04-03 14:05:50 +0300

    Done

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -8,20 +8,26 @@
 function averageMark(a, g, p) {
 	
     // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let averageGrade = (a + g + p) / 3;
+	
+	return  averageGrade // ваш ответ;
 }
 
 function sayHello(userName) {
 	
     // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	message = `Привет ${userName}`;
+	return message // ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
+	let x = 2; 
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result // ваш ответ;
 }
