6bc3938a30ce3702057e7ce7086a2a687831276e
Dmitry Vinogradov <pierrebernard@MacBook-Air-Pierre-Bernard.local>
2021-04-22 13:18:31 +0300

    2.2. Homework

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,8 +1,41 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function () {
+    let string = this;
+    let orderedLetters = [];
+    string = string.toLowerCase();
+    orderedLetters = string.split('');
+    let len = orderedLetters.length;
+    let reversedLetters = [];
+
+    for (i = 0; i < len; i++) {
+        if (orderedLetters[i] === " ") {
+            orderedLetters.splice(i, 1);
+        }
+    }
+    len = orderedLetters.length;
+    for (i = 0; i < len; i++) {
+        reversedLetters.unshift(orderedLetters[i]);
+    }
+    let result = true;;
+    for (i = 0; i < len; i++) {
+if (orderedLetters[i] != reversedLetters[i]) {
+    result = false;
+    return result
+}
+}
+return result
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    len = marks.length;
+    let sumOfMarks = 0;
+    let average;
+    for (i = 0; i < len; i++) {
+    sumOfMarks += marks[i];
+    }
+    average = sumOfMarks / len;
+    result = Math.round(average);
+    return result
 }
 
 function checkBirthday(birthday) {
