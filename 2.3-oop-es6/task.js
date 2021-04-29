f69c78827bc81f797b38fe15724c93e54f331276
Dmitry Vinogradov <pierrebernard@MacBook-Air-Pierre-Bernard.local>
2021-04-29 10:59:40 +0300

    Homework 2.3.

---
+++ 2.3-oop-es6/task.js
@@ -0,0 +1,111 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.type = null;
+        this.state = 100;
+    }
+
+    fix() {
+        return this.state = 1.5 * this.state;
+    }
+
+    set state(arg) {
+        if (arg > 100) {
+            this._state = 100;
+        }
+
+        if (arg < 0) {
+            this._state = 0;
+        }
+
+        if (arg <= 100 && arg >= 0) {
+            this._state = arg;
+        }
+    }
+
+    get state() {
+
+        return this._state;
+
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount)
+        this.type = 'book';
+        this.author = author;
+    }
+}
+
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount)
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount)
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount)
+        this.type = 'detective';
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+    findBookBy(type, value) {
+        for (let i = 0; i < this.books.length; i++) {
+            let checkedObject = this.books[i];
+            for (let prop in checkedObject) {
+                if (prop === type && checkedObject[prop] === value) {
+                    return checkedObject
+                }
+            }
+        }
+
+        return null
+    }
+    giveBookByName(bookName) {
+        let result = null;
+        let counter;
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                result = this.books[i];
+                counter = i;
+            }
+        }
+
+        if (counter) {
+            this.books.splice(counter, 1)
+        }
+
+        return result
+    }
+}
+
